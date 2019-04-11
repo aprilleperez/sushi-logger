240e2e2dbd4ab8530f4a86e69291f93916a88bb7
aprilleperez <aprillesakura@gmail.com>
2019-04-10 17:28:29 -0700

    added jaws_db in connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,29 @@
 var mysql = require("mysql");
 
 // Set up connection to mysql db
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password", // try hiding this?
-  database: "sushi_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password", // try hiding this?
+//   database: "sushi_db"
+// });
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'password',
+		database: 'sushi_db'
+	})
+};
 
 // Start connection
 connection.connect(function(err) {
